0584e4176fbeda52e56ec681845e61637486faff
oliver <ethansjmoon@gmail.com>
2024-10-30 19:47:42 -0400

    update from GAS at Wed Oct 30 2024 19:47:41 GMT-0400 (Eastern Daylight Time)

--- hotlist.js
+++ hotlist.js
@@ -1,9 +1,9 @@
 var json = {
     "code": "/** \n **IMPORTANT**\n **This Is not me being political \n **This Is not me Encouraging you to vote for him\n **This Is not me Encouraging you to vote against him\n **So Don't be thinking this is me doing anything \n **This is the First Idea I got when I thought of a new Derpachu to make\n * \n * \n * Oct 30 5:50pm I log on to make something find out this is 2ND on the HOTLIST HOW\n * Oct 30 7:10pm I AM 1ST ON THE HOTLIST FOR THE FIRST TIME EVER YAAAAAAAAAAAAAAAAYYYY\n\n              Here Is my Subpage\n              if you wanna sub\n\nhttps://www.khanacademy.org/computer-programming/my-subpage-yeaaaaaaaaaaaaaaaaaaaa/6431479248109568 \n\n\n**/\n{\nvar pixelSize = 6;\n\nvar palette = {\n    \".\" : color(0, 166, 255),\n    \"1\" : color(0, 0, 0),\n    \"2\" : color(252, 5, 5),\n    \"3\" : color(117, 69, 2),\n    \"4\" : color(251, 255, 0),\n    \"5\" : color(0, 7, 133),\n    \"6\" : color(207, 207, 207),\n    \"7\" : color(230, 172, 12),\n    \"8\" : color(240, 202, 32),\n    \"9\" : color(135, 83, 0),\n    \"a\" : color(255, 175, 84),\n    \"b\" : color(255, 255, 255)\n};\n\nvar bitmap = [\n     \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"..................0000000000000....................................\",\n    \".................00000.000.00000...................................\",\n    \"................000...........000..................................\",\n    \"..........00000000..............000000.............................\",\n    \"........00000........................00............................\",\n    \".......0000...........................00...........................\",\n    \".......000.............................00..........................\",\n    \".......00...............................0..........................\",\n    \".......00...............................0..........................\",\n    \".......00...............................0..........................\",\n    \".......00...............................0..........................\",\n    \"........00.............................00..........................\",\n    \".........00..........................000...........................\",\n    \"..........00000000..............0000000............................\",\n    \"................000............000.................................\",\n    \".................000........0000...................................\",\n    \"..................000000000000.....................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \"...................................................................\",\n    \".................................7777778...........................\",\n    \"............................7777777777778..........................\",\n    \"..........................77777777777777788........................\",\n    \"........................777777777777777778.........................\",\n    \"........................77777777777777788..........................\",\n    \".......................777777888888888811..........................\",\n    \".......................7778888114444441bb1.........................\",\n    \".......................7888441bb1411141b1b1........................\",\n    \"....................1..884441b1b1412141bbb1........................\",\n    \"...................141..14441bbb14414441bb1........................\",\n    \"..................1441..14441bb1444444441121.......................\",\n    \".................14441..14442114444444444221.......................\",\n    \"................1444441.14442244444444444441.......................\",\n    \"...............14444441..1555555566665555551.......................\",\n    \"..............1444444441.15555555522555555551......................\",\n    \"...............144444441.1555555552255555/551......................\",\n    \"................144444331155555/5522555/5/551......................\",\n    \"....aaaaaa.......144443331555/5/55225555/5551......................\",\n    \"...aa....a........144433315555/55522555555551......................\",\n    \"..aa.....a.........14411331555555522555555551......................\",\n    \".aa......a..........11..11155555556655555551.......................\",\n    \"aa.......a................15555555665555551........................\",\n    \".a.......a.................155555566555551.........................\",\n    \".a.......a..................1151111111151..........................\",\n    \".a.......a...................111......111..........................\",\n    \".a...99999999999999999999999999999999999999999999999999999999999999\",\n    \".a..999999999999999999999999999999999999999999999999999999999999999\",\n    \".a.9999999999999999999999999999999999999999999999999999999999999999\",\n    \".a99999999999999999999999999999999999999999999999999999999999999999\",\n    \".999999999999999999999999999999999999999999999999999999999999999999\",\n    \"9999999999999999999999999999999999999999999999999999999999999999999\",\n\n\n\n\n    ];\n\nnoStroke(); \nfor (var i = 0; i < bitmap.length; i++) {\n    for (var j = 0; j < bitmap[i].length; j++) {\n        fill(palette[bitmap[i].charAt(j)]);\n        rect(j * pixelSize, i * pixelSize, pixelSize, pixelSize);\n    }\n    \n    \n}\n\n\n\nvar leftX = 147;\nvar rightX = 270;\nfill(255, 255, 255);\nellipse(leftX, 100, 126, 97);\nellipse(leftX+62, 100, 70, 60);\nellipse(leftX-62, 100, 70, 60);\n\n}",
     "title": "Trumpachu *read description*",
-    "votes": 32,
+    "votes": 33,
     "created": "12 hours ago",
-    "updated": "31 minutes ago",
+    "updated": "36 minutes ago",
     "type": "PJS",
     "author": {
         "name": "HACK",
@@ -51,7 +51,7 @@ var json = {
                         "text": "I would get banned if I did that"
                     },
                     {
-                        "date": "33 minutes ago",
+                        "date": "38 minutes ago",
                         "author": {
                             "name": "Danomi42",
                             "id": "kaid_1045282636896433254069987",
@@ -60,7 +60,7 @@ var json = {
                         "text": "<code>@<b>HACK</b></code> | Wait, really? Why?"
                     },
                     {
-                        "date": "23 minutes ago",
+                        "date": "28 minutes ago",
                         "author": {
                             "name": "AlIEN",
                             "id": "kaid_213001050410218690794137",
@@ -69,7 +69,7 @@ var json = {
                         "text": "@Eyeinthesky highly innapropriatte"
                     },
                     {
-                        "date": "17 minutes ago",
+                        "date": "22 minutes ago",
                         "author": {
                             "name": "HACK",
                             "id": "kaid_6815067100354522609320825",
@@ -78,7 +78,7 @@ var json = {
                         "text": "yeah very"
                     },
                     {
-                        "date": "14 minutes ago",
+                        "date": "19 minutes ago",
                         "author": {
                             "name": "Danomi42",
                             "id": "kaid_1045282636896433254069987",
@@ -87,7 +87,7 @@ var json = {
                         "text": "hmmm...<br><br>I didn't say anything"
                     },
                     {
-                        "date": "13 minutes ago",
+                        "date": "18 minutes ago",
                         "author": {
                             "name": "AlIEN",
                             "id": "kaid_213001050410218690794137",
@@ -96,7 +96,7 @@ var json = {
                         "text": "we know, i was referring to the comment we are well commenting on. I think it would be best to close this thread. No one else respond"
                     },
                     {
-                        "date": "12 minutes ago",
+                        "date": "17 minutes ago",
                         "author": {
                             "name": "HACK",
                             "id": "kaid_6815067100354522609320825",
@@ -129,7 +129,7 @@ var json = {
                         "text": "yeah I'm not trying to be political because I only 14 and because elections coming up and because I'm not trying to get banned AGAIN"
                     },
                     {
-                        "date": "30 minutes ago",
+                        "date": "35 minutes ago",
                         "author": {
                             "name": "Danomi42",
                             "id": "kaid_1045282636896433254069987",
@@ -138,7 +138,7 @@ var json = {
                         "text": "<code>@<b>honeyghost</b></code> | Top 10 quotes of 2024: <em>\"The surest way to become a meme is to become the President of the United States.\"</em> - <code>honeyghost</code>'s brother (whoever it may be)"
                     },
                     {
-                        "date": "27 minutes ago",
+                        "date": "32 minutes ago",
                         "author": {
                             "name": "HACK",
                             "id": "kaid_6815067100354522609320825",
@@ -173,7 +173,7 @@ var json = {
                 ]
             },
             {
-                "replyCount": 2,
+                "replyCount": 4,
                 "votes": 6,
                 "date": "8 hours ago",
                 "author": {
@@ -195,13 +195,31 @@ var json = {
                         "text": "lol that would be hilarious"
                     },
                     {
-                        "date": "32 minutes ago",
+                        "date": "37 minutes ago",
                         "author": {
                             "name": "Danomi42",
                             "id": "kaid_1045282636896433254069987",
                             "avatar": "/images/avatars/svg/primosaur-sapling.svg"
                         },
                         "text": "Try saying that ten times fast..."
+                    },
+                    {
+                        "date": "2 minutes ago",
+                        "author": {
+                            "name": "HACK",
+                            "id": "kaid_6815067100354522609320825",
+                            "avatar": "/images/avatars/svg/cs-ohnoes.svg"
+                        },
+                        "text": "that's not possible"
+                    },
+                    {
+                        "date": "a few seconds ago",
+                        "author": {
+                            "name": "Danomi42",
+                            "id": "kaid_1045282636896433254069987",
+                            "avatar": "/images/avatars/svg/primosaur-sapling.svg"
+                        },
+                        "text": "<b>Watch me.</b><br><br>George Washingchu George Washingchu George Washingchu George Washingchu George Washingchu George Washingchu George Washingchu George Washingchu George Washingchu George Washingchu.<br><br>easy"
                     }
                 ]
             },
@@ -235,8 +253,22 @@ var json = {
             },
             {
                 "replyCount": 0,
+                "votes": 2,
+                "date": "5 minutes ago",
+                "author": {
+                    "name": "Blocked by Taquavion",
+                    "id": "kaid_568819712822552226070124",
+                    "avatar": "/images/avatars/svg/cs-ohnoes.svg"
+                },
+                "text": "Convicted criminal pikachu noice",
+                "locked": false,
+                "pinned": false,
+                "replies": []
+            },
+            {
+                "replyCount": 0,
                 "votes": 1,
-                "date": "34 minutes ago",
+                "date": "39 minutes ago",
                 "author": {
                     "name": "Danomi42",
                     "id": "kaid_1045282636896433254069987",
@@ -270,7 +302,7 @@ var json = {
                         "text": "I had the ears when I first made it but it looked insanely weird because it was just a floating black triangle for both ears so I removed them."
                     },
                     {
-                        "date": "8 hours ago",
+                        "date": "9 hours ago",
                         "author": {
                             "name": "HACK",
                             "id": "kaid_6815067100354522609320825",
@@ -319,7 +351,7 @@ var json = {
             {
                 "replyCount": 10,
                 "votes": 1,
-                "date": "10 hours ago",
+                "date": "11 hours ago",
                 "author": {
                     "name": "Pro0planter",
                     "id": "kaid_1863950377056983766116587",
@@ -402,7 +434,7 @@ var json = {
                         "text": "also wanna participate in my competition"
                     },
                     {
-                        "date": "30 minutes ago",
+                        "date": "35 minutes ago",
                         "author": {
                             "name": "HACK",
                             "id": "kaid_6815067100354522609320825",
@@ -411,7 +443,7 @@ var json = {
                         "text": "Maybe :)"
                     },
                     {
-                        "date": "30 minutes ago",
+                        "date": "35 minutes ago",
                         "author": {
                             "name": "HACK",
                             "id": "kaid_6815067100354522609320825",
@@ -468,19 +500,19 @@ var json = {
                 "votes": 1,
                 "date": "a few seconds ago",
                 "author": {
-                    "name": "Blocked by Taquavion",
-                    "id": "kaid_568819712822552226070124",
-                    "avatar": "/images/avatars/svg/cs-ohnoes.svg"
+                    "name": "SquiirelBoyMO",
+                    "id": "kaid_773398490746126089688872",
+                    "avatar": "/images/avatars/svg/leaf-grey.svg"
                 },
-                "text": "Convicted criminal pikachu noice",
+                "text": "You know what, Im not scared of a strike. I am just practicing freedom of speech. \"Give me liberty or give me death!!\"<br><br>#MakeAmericaGreatAgain!!",
                 "locked": false,
                 "pinned": false,
                 "replies": []
             },
             {
-                "replyCount": 0,
+                "replyCount": 1,
                 "votes": 1,
-                "date": "3 minutes ago",
+                "date": "8 minutes ago",
                 "author": {
                     "name": "Luke Ellis",
                     "id": "kaid_8535468719137003545030723",
@@ -489,14 +521,24 @@ var json = {
                 "text": "To be honest, I can't believe this is #1 on the hotlist.<br>You did a great job making some people laugh, so I'll give you that. However, next time I would not recommend doing something like this.<br>lol. Great job.",
                 "locked": false,
                 "pinned": false,
-                "replies": []
+                "replies": [
+                    {
+                        "date": "a minute ago",
+                        "author": {
+                            "name": "HACK",
+                            "id": "kaid_6815067100354522609320825",
+                            "avatar": "/images/avatars/svg/cs-ohnoes.svg"
+                        },
+                        "text": "Why would you recommend that"
+                    }
+                ]
             }
         ],
         "questions": [
             {
-                "replyCount": 1,
+                "replyCount": 2,
                 "votes": 0,
-                "date": "4 hours ago",
+                "date": "5 hours ago",
                 "author": {
                     "name": "Ts lame ash",
                     "id": "kaid_524552937178100337976251",
@@ -505,13 +547,22 @@ var json = {
                 "text": "trump 2024 fuh nga",
                 "replies": [
                     {
-                        "date": "13 minutes ago",
+                        "date": "18 minutes ago",
                         "author": {
                             "name": "AlIEN",
                             "id": "kaid_213001050410218690794137",
                             "avatar": "/images/avatars/svg/marcimus-purple.svg"
                         },
                         "text": "Again Innapropriatte"
+                    },
+                    {
+                        "date": "3 minutes ago",
+                        "author": {
+                            "name": "HACK",
+                            "id": "kaid_6815067100354522609320825",
+                            "avatar": "/images/avatars/svg/cs-ohnoes.svg"
+                        },
+                        "text": "<b>NO more commenting  on THIS THREAD</b>"
                     }
                 ],
                 "answers": []
