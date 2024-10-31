6832cfec8298b06354a4d675de0a632964a24a18
oliver <ethansjmoon@gmail.com>
2024-10-31 06:12:39 -0400

    update from GAS at Thu Oct 31 2024 06:12:39 GMT-0400 (Eastern Daylight Time)

--- hotlist.js
+++ hotlist.js
@@ -42,7 +42,7 @@ var json = {
                 "pinned": false,
                 "replies": [
                     {
-                        "date": "14 hours ago",
+                        "date": "15 hours ago",
                         "author": {
                             "name": "Mathlete11",
                             "id": "kaid_4902531429433401500771997",
@@ -66,7 +66,7 @@ var json = {
                 "pinned": false,
                 "replies": [
                     {
-                        "date": "14 hours ago",
+                        "date": "15 hours ago",
                         "author": {
                             "name": "Mathlete11",
                             "id": "kaid_4902531429433401500771997",
@@ -176,7 +176,7 @@ var json = {
             {
                 "replyCount": 5,
                 "votes": 4,
-                "date": "10 hours ago",
+                "date": "11 hours ago",
                 "author": {
                     "name": "Luke Ellis",
                     "id": "kaid_8535468719137003545030723",
@@ -376,7 +376,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 2,
-                "date": "14 hours ago",
+                "date": "15 hours ago",
                 "author": {
                     "name": "Mathlete11",
                     "id": "kaid_4902531429433401500771997",
@@ -513,7 +513,7 @@ var json = {
                         "text": "what? he's wearing a tuxedo"
                     },
                     {
-                        "date": "19 hours ago",
+                        "date": "20 hours ago",
                         "author": {
                             "name": "Pro0planter",
                             "id": "kaid_1863950377056983766116587",
@@ -612,7 +612,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 0,
-                "date": "10 hours ago",
+                "date": "11 hours ago",
                 "author": {
                     "name": "Blocked by Taquavion",
                     "id": "kaid_568819712822552226070124",
