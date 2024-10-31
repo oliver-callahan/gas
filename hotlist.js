e000fa90b33d11a92c7b0005edb2455859ccc515
oliver <ethansjmoon@gmail.com>
2024-10-31 03:37:40 -0400

    update from GAS at Thu Oct 31 2024 03:37:39 GMT-0400 (Eastern Daylight Time)

--- hotlist.js
+++ hotlist.js
@@ -3,7 +3,7 @@ var json = {
     "title": "Trumpachu *read description*",
     "votes": 37,
     "created": "20 hours ago",
-    "updated": "7 hours ago",
+    "updated": "8 hours ago",
     "type": "PJS",
     "author": {
         "name": "HACK",
@@ -148,7 +148,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 4,
-                "date": "12 hours ago",
+                "date": "13 hours ago",
                 "author": {
                     "name": "Firebrand (offline)",
                     "id": "kaid_423049852746380987692804",
@@ -214,7 +214,7 @@ var json = {
                         "text": "I know I have flagged most of the \"not good\" comments and told people not to comment on the thread of them so yeah I probably wont but people want me to make a George Washington one so I might make that one"
                     },
                     {
-                        "date": "7 hours ago",
+                        "date": "8 hours ago",
                         "author": {
                             "name": "HACK",
                             "id": "kaid_6815067100354522609320825",
