ea36dbccdef2f799af52ed7f3470cbd1452b2fbc
oliver <ethansjmoon@gmail.com>
2024-11-01 12:27:40 -0400

    update from GAS at Fri Nov 01 2024 12:27:39 GMT-0400 (Eastern Daylight Time)

--- hotlist.js
+++ hotlist.js
@@ -85,7 +85,7 @@ var json = {
                         "text": "Yes, but a holiday's a holiday :P"
                     },
                     {
-                        "date": "18 hours ago",
+                        "date": "19 hours ago",
                         "author": {
                             "name": "duke",
                             "id": "kaid_351465532815782433620675",
@@ -126,7 +126,7 @@ var json = {
             {
                 "replyCount": 6,
                 "votes": 3,
-                "date": "18 hours ago",
+                "date": "19 hours ago",
                 "author": {
                     "name": "The Former Wizard of Oz",
                     "id": "kaid_981854986090143486946241",
@@ -272,7 +272,7 @@ var json = {
                         "text": "The subpage works I guess!"
                     },
                     {
-                        "date": "18 hours ago",
+                        "date": "19 hours ago",
                         "author": {
                             "name": "SwankyMan™",
                             "id": "kaid_82898098000024228278987",
@@ -281,7 +281,7 @@ var json = {
                         "text": "At that point, It works too well.<br><br>ig it's what you should expect from a KA user who's been on here for more than 2 years and has extremely well made games/programs."
                     },
                     {
-                        "date": "18 hours ago",
+                        "date": "19 hours ago",
                         "author": {
                             "name": "Bearkirb314🐻‍❄️",
                             "id": "kaid_375460112550893828689953",
@@ -294,7 +294,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "7 minutes ago",
+                "date": "12 minutes ago",
                 "author": {
                     "name": "cb (usersniper)",
                     "id": "kaid_197781135646454512969328",
@@ -337,7 +337,7 @@ var json = {
                         "text": "Thanks :)"
                     },
                     {
-                        "date": "18 hours ago",
+                        "date": "19 hours ago",
                         "author": {
                             "name": "LemonTurtle",
                             "id": "kaid_26727758302107548837304",
@@ -346,7 +346,7 @@ var json = {
                         "text": "Me: *thinks hard.<br>Me: Aha!<br><br>\"Yes, BUT I said first, FIRST! Mwahahaha!\" XD"
                     },
                     {
-                        "date": "18 hours ago",
+                        "date": "19 hours ago",
                         "author": {
                             "name": "duke",
                             "id": "kaid_351465532815782433620675",
@@ -355,7 +355,7 @@ var json = {
                         "text": "Yes you were first to say first. I was first to vote :P"
                     },
                     {
-                        "date": "18 hours ago",
+                        "date": "19 hours ago",
                         "author": {
                             "name": "LemonTurtle",
                             "id": "kaid_26727758302107548837304",
@@ -382,7 +382,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "27 minutes ago",
+                "date": "32 minutes ago",
                 "author": {
                     "name": "HB_the_Pencil (semi-retired)",
                     "id": "kaid_412656070256786668848958",
@@ -438,7 +438,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "an hour ago",
+                "date": "2 hours ago",
                 "author": {
                     "name": "Zidane Medina",
                     "id": "kaid_4593991067080431920712749",
@@ -506,7 +506,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 2,
-                "date": "18 hours ago",
+                "date": "19 hours ago",
                 "author": {
                     "name": "Jake K.",
                     "id": "kaid_103000698141866290580261",
@@ -518,7 +518,7 @@ var json = {
                     {
                         "replyCount": 0,
                         "votes": 2,
-                        "date": "18 hours ago",
+                        "date": "19 hours ago",
                         "author": {
                             "name": "Bearkirb314🐻‍❄️",
                             "id": "kaid_375460112550893828689953",
