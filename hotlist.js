94a97948c3d95489d75c1a7fa5962860097e2ea1
oliver <ethansjmoon@gmail.com>
2024-11-02 12:12:38 -0400

    update from GAS at Sat Nov 02 2024 12:12:37 GMT-0400 (Eastern Daylight Time)

--- hotlist.js
+++ hotlist.js
@@ -98,7 +98,7 @@ var json = {
                         "text": "On the contrary my dear friend, I beg to differ. You misspelled the word milk, Mr Jake K's spelling is an immaculate epitome of perfection. While your spelling of the word milk is quite un-elementary and absurd in such a context as this. In fact, the very idea of spelling milk with a \"e\" instead of an \"i\" is quite uncommendable and unsatisfactory, and such an example is quite unbeneficial and unbecoming to the general audience of adolescents on this website. Also, to further the charges brought before you this day, I bring to attention the second misspelling of the word misspelled, which, while you may have missed this offence. It is quite as bad as the misspelling of the word milk. <br><br>You have the right to remain silent <br><br><em>Nice to see you both around by the way ;)</em>"
                     },
                     {
-                        "date": "14 hours ago",
+                        "date": "15 hours ago",
                         "author": {
                             "name": "NL",
                             "id": "kaid_101904269317089495055459",
@@ -294,7 +294,7 @@ var json = {
                         "text": "it's in Chinese"
                     },
                     {
-                        "date": "13 hours ago",
+                        "date": "14 hours ago",
                         "author": {
                             "name": "WAHOO-Allwen ",
                             "id": "kaid_1280249000611495284384187",
