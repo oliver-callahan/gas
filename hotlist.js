ab0354153ac16f963bfda51f3b4fea5db770d019
oliver <ethansjmoon@gmail.com>
2024-11-01 21:57:37 -0400

    update from GAS at Fri Nov 01 2024 21:57:37 GMT-0400 (Eastern Daylight Time)

--- hotlist.js
+++ hotlist.js
@@ -121,7 +121,7 @@ var json = {
                         "text": "i think you mispled melk"
                     },
                     {
-                        "date": "24 minutes ago",
+                        "date": "29 minutes ago",
                         "author": {
                             "name": "Arrow",
                             "id": "kaid_5229809678324099512179597",
@@ -130,7 +130,7 @@ var json = {
                         "text": "On the contrary my dear friend, I beg to differ. You misspelled the word milk, Mr Jake K's spelling is an immaculate epitome of perfection. While your spelling of the word milk is quite un-elementary and absurd in such a context as this. In fact, the very idea of spelling milk with a \"e\" instead of an \"i\" is quite uncommendable and unsatisfactory, and such an example is quite unbeneficial and unbecoming to the general audience of adolescents on this website. Also, to further the charges brought before you this day, I bring to attention the second misspelling of the word misspelled, which, while you may have missed this offence. It is quite as bad as the misspelling of the word milk. <br><br>You have the right to remain silent <br><br><em>Nice to see you both around by the way ;)</em>"
                     },
                     {
-                        "date": "10 minutes ago",
+                        "date": "15 minutes ago",
                         "author": {
                             "name": "NL",
                             "id": "kaid_101904269317089495055459",
@@ -264,7 +264,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 2,
-                "date": "4 hours ago",
+                "date": "5 hours ago",
                 "author": {
                     "name": "Samurai Warrior™ ✝ (Offline)",
                     "id": "kaid_333534297788735128142174",
@@ -330,7 +330,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 2,
-                "date": "9 hours ago",
+                "date": "10 hours ago",
                 "author": {
                     "name": "p0tat0s_gUn",
                     "id": "kaid_9504700940326785358604450",
