53b9b86f595f45390c1ebda74ed0fbbd5c5d6fb0
oliver <ethansjmoon@gmail.com>
2024-11-02 03:27:38 -0400

    update from GAS at Sat Nov 02 2024 03:27:37 GMT-0400 (Eastern Daylight Time)

--- hotlist.js
+++ hotlist.js
@@ -59,7 +59,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 5,
-                "date": "16 hours ago",
+                "date": "17 hours ago",
                 "author": {
                     "name": "Luke Ellis",
                     "id": "kaid_8535468719137003545030723",
@@ -283,7 +283,7 @@ var json = {
                         "text": "yea its in Chinese :p lol I didn't make this don't credit me"
                     },
                     {
-                        "date": "4 hours ago",
+                        "date": "5 hours ago",
                         "author": {
                             "name": "kitty mascot",
                             "id": "kaid_1066778980955332043559618",
