dac7d5bb93ac5e653833af96803608e93185f251
oliver <ethansjmoon@gmail.com>
2024-11-03 07:22:39 -0500

    update from GAS at Sun Nov 03 2024 07:22:39 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -130,7 +130,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 3,
-                "date": "16 hours ago",
+                "date": "17 hours ago",
                 "author": {
                     "name": "Mushy Avocado",
                     "id": "kaid_889538221957342292210697",
@@ -452,7 +452,7 @@ var json = {
                         "text": "20 levels. You can see them all by pressing on the levels button",
                         "replies": [
                             {
-                                "date": "14 hours ago",
+                                "date": "15 hours ago",
                                 "author": {
                                     "name": "TheGimGamer",
                                     "id": "kaid_703826623747117899019912",
