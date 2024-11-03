c49abf32bb2cd3eacb3d0285e7b87f3f88a4f548
oliver <ethansjmoon@gmail.com>
2024-11-03 01:37:45 -0500

    update from GAS at Sun Nov 03 2024 01:37:43 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -55,7 +55,7 @@ var json = {
             {
                 "replyCount": 3,
                 "votes": 6,
-                "date": "13 hours ago",
+                "date": "14 hours ago",
                 "author": {
                     "name": "𝐂𝐥𝐨𝐯𝐞𝐫",
                     "id": "kaid_2287805493149654773942312",
@@ -108,7 +108,7 @@ var json = {
                 "pinned": false,
                 "replies": [
                     {
-                        "date": "13 hours ago",
+                        "date": "14 hours ago",
                         "author": {
                             "name": "Quantum Cat",
                             "id": "kaid_9428127706426004539463954",
@@ -216,7 +216,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 2,
-                "date": "14 hours ago",
+                "date": "15 hours ago",
                 "author": {
                     "name": "SnackerDavis",
                     "id": "kaid_889584744100087037395082",
@@ -240,7 +240,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "14 hours ago",
+                "date": "15 hours ago",
                 "author": {
                     "name": "kitty mascot",
                     "id": "kaid_1066778980955332043559618",
@@ -364,7 +364,7 @@ var json = {
                         "text": "Thanks!<br><br>Ha, so you were one of the ones who voted it"
                     },
                     {
-                        "date": "13 hours ago",
+                        "date": "14 hours ago",
                         "author": {
                             "name": "Eyeinthesky",
                             "id": "kaid_6136710333637048514584747",
@@ -393,7 +393,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "18 hours ago",
+                "date": "19 hours ago",
                 "author": {
                     "name": "TheGimGamer",
                     "id": "kaid_703826623747117899019912",
