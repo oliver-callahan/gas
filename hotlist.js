f99764edfbd198d25347212dcae9e889e0219f1a
oliver <ethansjmoon@gmail.com>
2024-11-09 11:17:41 -0500

    update from GAS at Sat Nov 09 2024 11:17:40 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -442,7 +442,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 2,
-                "date": "21 hours ago",
+                "date": "a day ago",
                 "author": {
                     "name": "$c@∫∑∑$",
                     "id": "kaid_579183804717579680253074",
