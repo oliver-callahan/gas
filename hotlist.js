60efd688285bb50ba71569a74a3b01b19f1131c4
oliver <ethansjmoon@gmail.com>
2024-11-10 22:57:38 -0500

    update from GAS at Sun Nov 10 2024 22:57:37 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -261,7 +261,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "an hour ago",
+                "date": "2 hours ago",
                 "author": {
                     "name": "Radar",
                     "id": "kaid_3902988618718040904060736",
