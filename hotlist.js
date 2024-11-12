542743ce0d337d72bf95e93344585b1c3540c440
oliver <ethansjmoon@gmail.com>
2024-11-12 17:07:45 -0500

    update from GAS at Tue Nov 12 2024 17:07:44 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -94,7 +94,7 @@ var json = {
                         "text": "oh my gosh XD"
                     },
                     {
-                        "date": "4 hours ago",
+                        "date": "5 hours ago",
                         "author": {
                             "name": "JSCoder",
                             "id": "kaid_3032679493880386290676439",
@@ -103,7 +103,7 @@ var json = {
                         "text": "I guess I was no the only one who though self portrait"
                     },
                     {
-                        "date": "4 hours ago",
+                        "date": "5 hours ago",
                         "author": {
                             "name": "kitty mascot",
                             "id": "kaid_1066778980955332043559618",
@@ -316,7 +316,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 5,
-                "date": "21 hours ago",
+                "date": "a day ago",
                 "author": {
                     "name": "WESTY METZY",
                     "id": "kaid_7615172111979300259760122",
@@ -606,7 +606,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "6 hours ago",
+                "date": "7 hours ago",
                 "author": {
                     "name": "Imtabest41",
                     "id": "kaid_335094101544484503242017",
