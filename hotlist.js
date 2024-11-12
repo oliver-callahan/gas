f429e953f2b435babeff43c33ebc84aee0e6aaa4
oliver <ethansjmoon@gmail.com>
2024-11-11 21:57:41 -0500

    update from GAS at Mon Nov 11 2024 21:57:40 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -174,7 +174,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 4,
-                "date": "6 hours ago",
+                "date": "7 hours ago",
                 "author": {
                     "name": "Bearkirb314🐻‍❄️",
                     "id": "kaid_375460112550893828689953",
@@ -396,7 +396,7 @@ var json = {
                         "text": "@Bravo Bravo I have heard about class's. But while on ka I try to make the best of what I have :P<br>haha having never <em>really</em> used inheritance I wouldn't know but ig your right :P<br><br>@Mush yeah ES6 looks sweet. I just haven't had much access to any other coding sight(that uses ES6)"
                     },
                     {
-                        "date": "9 hours ago",
+                        "date": "10 hours ago",
                         "author": {
                             "name": "Mushy Avocado",
                             "id": "kaid_889538221957342292210697",
@@ -405,7 +405,7 @@ var json = {
                         "text": "If you use my Khan Academy exporter you can use ES6 syntax while at the same time writing normal PJS code. ES6 works in the HTML environment :)"
                     },
                     {
-                        "date": "9 hours ago",
+                        "date": "10 hours ago",
                         "author": {
                             "name": "bravo bravo",
                             "id": "kaid_3550040499775075061747637",
@@ -526,7 +526,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 0,
-                "date": "7 hours ago",
+                "date": "8 hours ago",
                 "author": {
                     "name": "JJP",
                     "id": "kaid_874785892689340729331212",
