8a2b2b25072f6220f77a36d114b8d1edc0886f39
oliver <ethansjmoon@gmail.com>
2024-11-12 22:32:46 -0500

    update from GAS at Tue Nov 12 2024 22:32:46 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -59,7 +59,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 5,
-                "date": "19 hours ago",
+                "date": "20 hours ago",
                 "author": {
                     "name": "Xatnys",
                     "id": "kaid_3144599174897393521218325",
@@ -368,7 +368,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "2 hours ago",
+                "date": "3 hours ago",
                 "author": {
                     "name": "HAMBURGER RiDER",
                     "id": "kaid_410991944085535280495507",
@@ -382,7 +382,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 1,
-                "date": "15 hours ago",
+                "date": "16 hours ago",
                 "author": {
                     "name": "HB_the_Pencil (semi-retired)",
                     "id": "kaid_412656070256786668848958",
@@ -768,7 +768,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 1,
-                "date": "7 hours ago",
+                "date": "8 hours ago",
                 "author": {
                     "name": "cg048",
                     "id": "kaid_956853891251979880724349",
