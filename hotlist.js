4e84967fba6b2e25311d7d9dc0530b28a2ee5889
oliver <ethansjmoon@gmail.com>
2024-11-12 23:32:48 -0500

    update from GAS at Tue Nov 12 2024 23:32:47 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -59,7 +59,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 5,
-                "date": "20 hours ago",
+                "date": "21 hours ago",
                 "author": {
                     "name": "Xatnys",
                     "id": "kaid_3144599174897393521218325",
@@ -368,7 +368,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 1,
-                "date": "3 hours ago",
+                "date": "4 hours ago",
                 "author": {
                     "name": "HAMBURGER RiDER",
                     "id": "kaid_410991944085535280495507",
@@ -379,7 +379,7 @@ var json = {
                 "pinned": false,
                 "replies": [
                     {
-                        "date": "28 minutes ago",
+                        "date": "33 minutes ago",
                         "author": {
                             "name": "Radar",
                             "id": "kaid_3902988618718040904060736",
@@ -392,7 +392,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 1,
-                "date": "16 hours ago",
+                "date": "17 hours ago",
                 "author": {
                     "name": "HB_the_Pencil (semi-retired)",
                     "id": "kaid_412656070256786668848958",
@@ -754,7 +754,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 1,
-                "date": "7 hours ago",
+                "date": "8 hours ago",
                 "author": {
                     "name": "YoshiDude",
                     "id": "kaid_8691532869042265101128670",
@@ -778,7 +778,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 1,
-                "date": "8 hours ago",
+                "date": "9 hours ago",
                 "author": {
                     "name": "cg048",
                     "id": "kaid_956853891251979880724349",
