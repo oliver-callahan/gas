4d3f72f34ff1a9dc69a887e331abb3f4a5fba872
oliver <ethansjmoon@gmail.com>
2024-11-15 07:07:39 -0500

    update from GAS at Fri Nov 15 2024 07:07:38 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -20,7 +20,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 4,
-                "date": "13 hours ago",
+                "date": "14 hours ago",
                 "author": {
                     "name": "Cataclysmic Code 🌩",
                     "id": "kaid_820167475461480890635740",
@@ -58,7 +58,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 2,
-                "date": "14 hours ago",
+                "date": "15 hours ago",
                 "author": {
                     "name": "The one",
                     "id": "kaid_9809147761059943698111879",
@@ -133,7 +133,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 1,
-                "date": "13 hours ago",
+                "date": "14 hours ago",
                 "author": {
                     "name": "sugarnlight",
                     "id": "kaid_516497598968512440616556",
@@ -182,7 +182,7 @@ var json = {
                 "pinned": false,
                 "replies": [
                     {
-                        "date": "13 hours ago",
+                        "date": "14 hours ago",
                         "author": {
                             "name": "ASBackup",
                             "id": "kaid_714780036830891967670231",
@@ -191,7 +191,7 @@ var json = {
                         "text": "still reading this lol<br>i don't like feta either tbh i like any other cheeses tho :)"
                     },
                     {
-                        "date": "13 hours ago",
+                        "date": "14 hours ago",
                         "author": {
                             "name": "ASBackup",
                             "id": "kaid_714780036830891967670231",
@@ -218,7 +218,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "9 minutes ago",
+                "date": "14 minutes ago",
                 "author": {
                     "name": "Panda15",
                     "id": "kaid_9155426718383676698036057",
