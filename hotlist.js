b5be29156aedc372a59ed4b42ba1a4b55c033f26
oliver <ethansjmoon@gmail.com>
2024-11-15 00:47:37 -0500

    update from GAS at Fri Nov 15 2024 00:47:36 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -105,7 +105,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "3 hours ago",
+                "date": "4 hours ago",
                 "author": {
                     "name": "TH3 ROUG3 L3G3ND™",
                     "id": "kaid_2809101008758048501324524",
@@ -119,7 +119,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 1,
-                "date": "an hour ago",
+                "date": "2 hours ago",
                 "author": {
                     "name": "Duke",
                     "id": "kaid_351465532815782433620675",
