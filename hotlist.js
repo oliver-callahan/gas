b423d3bfb05d84aa987411768eb990d6e66a4ffe
oliver <ethansjmoon@gmail.com>
2024-11-15 09:07:38 -0500

    update from GAS at Fri Nov 15 2024 09:07:37 GMT-0500 (Eastern Standard Time)

--- hotlist.js
+++ hotlist.js
@@ -20,7 +20,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 6,
-                "date": "15 hours ago",
+                "date": "16 hours ago",
                 "author": {
                     "name": "Cataclysmic Code 🌩",
                     "id": "kaid_820167475461480890635740",
@@ -72,7 +72,7 @@ var json = {
             {
                 "replyCount": 0,
                 "votes": 2,
-                "date": "16 hours ago",
+                "date": "17 hours ago",
                 "author": {
                     "name": "The one",
                     "id": "kaid_9809147761059943698111879",
@@ -133,7 +133,7 @@ var json = {
             {
                 "replyCount": 1,
                 "votes": 1,
-                "date": "15 hours ago",
+                "date": "16 hours ago",
                 "author": {
                     "name": "sugarnlight",
                     "id": "kaid_516497598968512440616556",
@@ -182,7 +182,7 @@ var json = {
                 "pinned": false,
                 "replies": [
                     {
-                        "date": "15 hours ago",
+                        "date": "16 hours ago",
                         "author": {
                             "name": "ASBackup",
                             "id": "kaid_714780036830891967670231",
@@ -191,7 +191,7 @@ var json = {
                         "text": "still reading this lol<br>i don't like feta either tbh i like any other cheeses tho :)"
                     },
                     {
-                        "date": "15 hours ago",
+                        "date": "16 hours ago",
                         "author": {
                             "name": "ASBackup",
                             "id": "kaid_714780036830891967670231",
